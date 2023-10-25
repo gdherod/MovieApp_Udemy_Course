1e7f4fdba8852e82ba43b5763b1fe93599c69634
Gary Hermosilla Rodríguez <ghermosilla@smu.cl>
2023-10-25 18:08:08 -0300

    feat: creating UI part 1

---
+++ app/src/main/java/com/example/movieapp/MovieFragment.kt
@@ -0,0 +1,14 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MovieFragment : Fragment(R.layout.fragment_movie) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
