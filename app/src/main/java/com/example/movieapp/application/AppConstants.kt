1e88bd23addea267c66c9b0cd102135490fd88ee
Gary Hermosilla Rodríguez <ghermosilla@smu.cl>
2023-10-27 18:33:49 -0300

    feat: added values to connect to api

---
+++ app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -0,0 +1,9 @@
+package com.example.movieapp.application
+
+object AppConstants {
+
+    //API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    const val API_KEY = "c280676686fe7e5496c37a7898139c4e"
+}
