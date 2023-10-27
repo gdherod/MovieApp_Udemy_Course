56260fb9c8fb51dad7a80dc062dcbf11533e43bc
Gary Hermosilla Rodríguez <ghermosilla@smu.cl>
2023-10-27 18:12:33 -0300

    feat: creating data layer

--- app/src/main/java/com/example/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/data/model/Movie.kt
@@ -1,5 +1,32 @@
 package com.example.movieapp.data.model
 
+import com.google.gson.annotations.SerializedName
+
+data class MovieList(
+    val results: List<Movie> = listOf()
+)
+
 data class Movie(
-    val id: Int = -1
+    val adult: Boolean = false,
+    @SerializedName("backdrop_path")
+    val backdropPath: String = "",
+    @SerializedName("genre_ids")
+    val genreIds: List<Int> = listOf(),
+    val id: Int = -1,
+    @SerializedName("original_language")
+    val originalLanguage: String = "",
+    @SerializedName("original_title")
+    val originalTitle: String = "",
+    val overview: String = "",
+    val popularity: Double = -1.0,
+    @SerializedName("poster_path")
+    val posterPath: String = "",
+    @SerializedName("release_date")
+    val releaseDate: String = "",
+    val title: String = "",
+    val video: Boolean = false,
+    @SerializedName("vote_average")
+    val voteAverage: Double = -1.0,
+    @SerializedName("vote_count")
+    val voteCount: Int = -1
 )
--- app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
@@ -1,4 +1,18 @@
 package com.example.movieapp.data.remote
 
+import com.example.movieapp.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList {
+        return MovieList()
+    }
 }
