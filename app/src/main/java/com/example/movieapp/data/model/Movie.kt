5eeb779a3e8e1675b8938e96cefff143795eb823
Gary Hermosilla Rodríguez <ghermosilla@smu.cl>
2023-12-12 16:22:19 -0300

    feat: added room entity

--- app/src/main/java/com/example/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/data/model/Movie.kt
@@ -1,5 +1,8 @@
 package com.example.movieapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 data class MovieList(
@@ -28,5 +31,37 @@ data class Movie(
     @SerializedName("vote_average")
     val voteAverage: Double = -1.0,
     @SerializedName("vote_count")
+    val voteCount: Int = -1
+)
+
+// Room
+
+@Entity
+data class MovieEntity(
+    @ColumnInfo(name = "adult")
+    val adult: Boolean = false,
+    @ColumnInfo(name = "backdrop_path")
+    val backdropPath: String = "",
+    @PrimaryKey
+    val id: Int = -1,
+    @ColumnInfo(name = "original_language")
+    val originalLanguage: String = "",
+    @ColumnInfo(name = "original_title")
+    val originalTitle: String = "",
+    @ColumnInfo(name = "overview")
+    val overview: String = "",
+    @ColumnInfo(name = "popularity")
+    val popularity: Double = -1.0,
+    @ColumnInfo(name = "poster_path")
+    val posterPath: String = "",
+    @ColumnInfo(name = "release_date")
+    val releaseDate: String = "",
+    @ColumnInfo(name = "title")
+    val title: String = "",
+    @ColumnInfo(name = "video")
+    val video: Boolean = false,
+    @ColumnInfo(name = "vote_average")
+    val voteAverage: Double = -1.0,
+    @ColumnInfo(name = "vote_count")
     val voteCount: Int = -1
 )
